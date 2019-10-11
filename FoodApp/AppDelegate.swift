276a87d0459b4c037870dbb3b7b5e1d163a72801
Lyes-ATEK <lyes.atek@gmail.com>
2019-10-11 15:58:55 +0200

    Initialize Project : Architecture And MainView

--- FoodApp/AppDelegate.swift
+++ FoodApp/AppDelegate.swift
@@ -16,7 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let navgiationController = UINavigationController()
+        let mainViewController = MainViewController()
+        navgiationController.pushViewController(mainViewController, animated: false)
+        window!.rootViewController = navgiationController
+        window!.makeKeyAndVisible()
         return true
     }
 
---
+++ FoodApp/Views/Controller/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  FoodApp
+//
+//  Created by Lyes ATEK on 11/10/2019.
+//  Copyright © 2019 Lyes ATEK. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController : UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
